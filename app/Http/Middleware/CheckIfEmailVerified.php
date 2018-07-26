cb39e9373b1fdf3f53094e7caf7b3c09468a8f59
zuiqiangqishao <727040917@qq.com>
2018-07-26 11:04:50 +0800

    邮箱验证中间件

---
+++ app/Http/Middleware/CheckIfEmailVerified.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckIfEmailVerified
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->user()->email_verified) {
+            //如果是AJAX请求，则通过JSON返回
+            if ($request->expectsJson()) {
+                return response()->json(['msg'=>'请先验证邮箱'], 400);
+            }
+            return redirect(route('email_verify_notice'));
+        }
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
+//首页
 Route::get('/', 'PagesController@root')->name('root');
 
+//用户登录注册
 Auth::routes();
+
+//未验证邮箱的重定向页面
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+    // 结束
+});
