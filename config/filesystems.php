3154d091343d15b1b7c8a7053e518323d9e83557
zuiqiangqishao <727040917@qq.com>
2018-07-26 22:47:23 +0800

    商品模型

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,13 +45,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => public_path('uploads'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads/',
             'visibility' => 'public',
         ],
 
