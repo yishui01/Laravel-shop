cf937b3f49b2f9e4b275da59c642c3b816074694
zuiqiangqishao <727040917@qq.com>
2018-07-28 23:09:09 +0800

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -17,5 +17,7 @@ Route::group([
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     //订单发货
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
+    //拒绝退款
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 
 });
