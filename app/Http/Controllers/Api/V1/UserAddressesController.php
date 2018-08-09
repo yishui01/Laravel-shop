0f38bc3e83b66bf01cbae7d05cc13613176bb21f
zuiqiangqishao <727040917@qq.com>
2018-08-09 22:46:07 +0800

    增加收货地址列表接口

--- app/Http/Controllers/Api/V1/UserAddressesController.php
+++ app/Http/Controllers/Api/V1/UserAddressesController.php
@@ -2,14 +2,37 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\Product;
+use App\Models\SocialInfo;
+use App\Models\User;
+use App\Models\UserAddress;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 class UserAddressesController extends Controller
 {
     public function index()
     {
-        $user = Auth::guard('api')->user(); //根据token解析出对应的用户
-        $user_address = ['123','456',];
-        return $this->response->array($user_address)->setStatusCode(201);
+        $social_user = Auth::guard('api')->user(); //根据token解析出对应的用户
+        //var_dump($titles = Product::pluck('键值','键名')->toArray());
+        $builder = UserAddress::query();
+        if ($social_user->user_id) {
+            //如果绑定了PC端的账号,找出所有的第三方账号对应的地址，与PC端账号的地址一起返回
+            $all_data = SocialInfo::pluck('type', 'id')
+                ->where('user_id', $social_user->user_id);
+
+            foreach ($all_data as $id=>$user_type) {
+                $builder->orWhere(function ($query) use ($id, $user_type){
+                    $query->where('user_id', $id)->where('user_type', $user_type);
+                });
+            }
+        }
+        $builder->orWhere(function ($query) use ($social_user){
+            $query->where('user_id', $social_user->user_id)->where('user_type', 'users');
+        });
+        $address = $builder->get();
+        foreach ($address as &$v) {
+            $v->full_address = $v->FullAddress;
+        }
+        return $this->response->array($address)->setStatusCode(201);
     }
 }
