a0f4381b3b225824de9adc09aa7e3bef0e1d5c48
zuiqiangqishao <727040917@qq.com>
2018-09-03 16:13:21 +0800

    bug fix

--- app/Observer/ProductSkuObserver.php
+++ app/Observer/ProductSkuObserver.php
@@ -8,10 +8,20 @@ use App\Models\ProductSku;
 class ProductSkuObserver
 {
 
-    public function saving(ProductSku $sku)
+    public function saved(ProductSku $sku)
+    {
+        $this->updateMinPrice($sku->product_id);
+    }
+
+    public function deleted($sku)
+    {
+        $this->updateMinPrice($sku->product_id);
+    }
+
+    protected function updateMinPrice($product_id)
     {
         //每次保存的时候，找出当前商品中价格最低的sku，更新到product表
-        $product = Product::find($sku->product_id);
+        $product = Product::find($product_id);
         $sku = $product->skus;
         $min = collect($sku)->min('price');
         $product->price = $min ?? 0;
