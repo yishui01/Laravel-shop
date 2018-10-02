0a31d58f12305e91333a633c60f97a99e3c5e52e
zuiqiangqishao <727040917@qq.com>
2018-10-02 16:56:35 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,8 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     //众筹商品下单
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+    //创建分期付款
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 
 
