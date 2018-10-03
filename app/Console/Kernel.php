d225cbae4ac59527bc4c4fc0365c22d467d3101a
zuiqiangqishao <727040917@qq.com>
2018-10-03 08:21:06 +0800

    计算逾期费

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,8 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
         // 每分钟执行一次
         $schedule->command('cron:finish-crowdfunding')->everyMinute();
+        // daily() 代表每天凌晨 00:00 执行
+        $schedule->command('cron:calculate-installment-fine')->daily();
     }
 
     /**
